3603bfa8ffe2cf45d5ec48abda6d9981d1b0636d
adolfo flores <adoflores@paypal.com>
2022-06-27 13:44:10 -0500

    Fix locales for new formats

--- server/components/auth-button/locales/index.js
+++ server/components/auth-button/locales/index.js
@@ -1,8 +1,15 @@
 /* @flow */
 import { locales } from './locales';
 
+// Handle old and new locale formats
+const newTodeprecatedLocales = {
+    'zh-hans': 'zh-cn',
+    'zh-hant': 'zh-tw'
+}
+
 export const selectLocalizedText = ({ inputLabel = 'login', locale }) => {
-    const selectedLocale = locales[locale.toLowerCase()] || locales['en-us'];
+    const depLocaleFormat = newTodeprecatedLocales[locale.toLowerCase()];
+    const selectedLocale = locales[depLocaleFormat || locale.toLowerCase()] || locales['en-us'];
     const selectedMessage = selectedLocale[inputLabel.toLowerCase()];
     return selectedMessage || selectedLocale.login;
 };
