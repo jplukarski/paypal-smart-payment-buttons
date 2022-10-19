476c67131cdcd3ce3065466b5baa2711c82ae638
Nate Bierdeman <nbierdeman@paypal.com>
2022-10-19 16:49:14 -0400

    Add FI_ID

--- src/button/button.js
+++ src/button/button.js
@@ -234,7 +234,7 @@ export function setupButton(opts : SetupButtonOptions) : ZalgoPromise<void> {
     const setupButtonLogsTask = setupButtonLogger({
         style, env, sdkVersion, sessionID, clientID, partnerAttributionID, commit, sdkCorrelationID,
         stickinessID, buttonCorrelationID, locale, merchantID, buttonSessionID, merchantDomain,
-        fundingSource, getQueriedEligibleFunding, buyerCountry, onShippingChange, experience });
+        fundingSource, getQueriedEligibleFunding, buyerCountry, onShippingChange, experience, wallet });
     const setupPaymentFlowsTask = setupPaymentFlows({ props, config, serviceData, components });
     const setupExportsTask = setupExports({ props, isEnabled, facilitatorAccessToken, fundingEligibility, merchantID });
 
