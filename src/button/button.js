131a2f09cad1f371bbb4d1753436bb40f9f338dd
Nate Bierdeman <nbierdeman@paypal.com>
2022-10-31 10:04:16 -0700

    Add FPTI_KEY.PRODUCT

--- src/button/button.js
+++ src/button/button.js
@@ -230,7 +230,7 @@ export function setupButton(opts : SetupButtonOptions) : ZalgoPromise<void> {
     const setupButtonLogsTask = setupButtonLogger({
         style, env, sdkVersion, sessionID, clientID, partnerAttributionID, commit, sdkCorrelationID,
         stickinessID, buttonCorrelationID, locale, merchantID, buttonSessionID, merchantDomain,
-        fundingSource, getQueriedEligibleFunding, buyerCountry, onShippingChange, experience, wallet, smartWalletOrderID });
+        fundingSource, getQueriedEligibleFunding, buyerCountry, onShippingChange, experience, wallet, smartWalletOrderID, enableOrdersApprovalSmartWallet });
     const setupPaymentFlowsTask = setupPaymentFlows({ props, config, serviceData, components });
     const setupExportsTask = setupExports({ props, isEnabled, facilitatorAccessToken, fundingEligibility, merchantID });
 
